af5a26db1f7ac244569d912fda4905cb736e9102
dayong <761385718@qq.com>
2019-05-16 21:01:22 +0800

    tab ok

---
+++ src/pages/Catalogue/Catalogue.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>catalogue</div>
+</template>
+
+<script>
+  export default {
+    name: "Catalogue"
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
---
+++ src切换tab完成/components/Footers/Footers.vue
@@ -0,0 +1,14 @@
+<template>
+ 
+	<div>Footer</div>
+</template>
+
+<script>
+  export default {
+    name: "Footers"
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
